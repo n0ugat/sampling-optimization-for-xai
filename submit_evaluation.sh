ccc6e8eda541ef8963ecba8a8cbf0c410aaabd80
Willz789 <williammistarz@gmail.com>
2025-04-03 20:35:32 +0200

    1000 sample results and hpc submit scripts

---
+++ submit_evaluation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#BSUB -q gpuv100
+#BSUB -J freqrise_digit_eval
+#BSUB -n 8
+#BSUB -W 01:00
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=500MB]"
+#BSUB -o outputs/freqrise_digit_eval%J.out
+#BSUB -e outputs/freqrise_digit_eval%J.err
+
+source ~/miniforge3/etc/profile.d/conda.sh
+conda activate freqrise
+
+lscpu
+echo "-----------------------------------------"
+python main_evaluation.py --labeltype digit --n_samples 1000
+

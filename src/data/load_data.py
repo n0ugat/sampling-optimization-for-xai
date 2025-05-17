67ec6bdc96b6040a3a02043476cf84f6ce9bcb43
Willz789 <williammistarz@gmail.com>
2025-05-17 11:55:40 +0200

    Fix to error with n_samples=1

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -8,7 +8,7 @@ def load_data(args):
     if args.dataset == 'synthetic':
         test_data, test_labels = synthetic_dataset_generator(args.n_samples, length=args.synth_sig_len, noiselevel=args.noise_level)
         test_data, test_labels = torch.tensor(test_data.reshape(args.n_samples, 1,1,-1)).float(), torch.tensor(test_labels).squeeze().long()
-        test_loader = DataLoader(TensorDataset(test_data, test_labels), batch_size=64)
+        test_loader = DataLoader(TensorDataset(test_data, test_labels.unsqueeze(0) if test_labels.dim() == 0 else test_labels), batch_size=64)
     elif args.dataset == 'AudioMNIST':
         test_dset = AudioNetDataset(args.data_path, True, 'test', splits = [0], labeltype = args.labeltype, subsample = args.n_samples, seed = 0, add_noise=False)  
         test_loader = DataLoader(test_dset, batch_size=10, shuffle=False)
